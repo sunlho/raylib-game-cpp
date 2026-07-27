6233e30f631387be4f6d288d4b9e60fd6a1c244f
sunlho <1784516575@qq.com>
2026-07-27 17:28:39 +0800

    refactor: 1

--- assets/Tiles/water.tsx
+++ assets/Tiles/water.tsx
@@ -1,16 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="water" tilewidth="32" tileheight="32" tilecount="8" columns="8">
- <image source="water.png" width="256" height="32"/>
+<tileset version="1.10" tiledversion="1.12.2" name="water" tilewidth="34" tileheight="24" spacing="2" margin="1" tilecount="8" columns="8">
+ <image source="water.png" width="288" height="26"/>
  <tile id="0">
   <animation>
-   <frame tileid="0" duration="200"/>
-   <frame tileid="1" duration="200"/>
-   <frame tileid="2" duration="200"/>
-   <frame tileid="3" duration="200"/>
-   <frame tileid="4" duration="200"/>
-   <frame tileid="5" duration="200"/>
-   <frame tileid="6" duration="200"/>
-   <frame tileid="7" duration="200"/>
+   <frame tileid="0" duration="300"/>
+   <frame tileid="1" duration="300"/>
+   <frame tileid="2" duration="300"/>
+   <frame tileid="3" duration="300"/>
+   <frame tileid="4" duration="300"/>
+   <frame tileid="5" duration="300"/>
+   <frame tileid="6" duration="300"/>
+   <frame tileid="7" duration="300"/>
   </animation>
  </tile>
 </tileset>
